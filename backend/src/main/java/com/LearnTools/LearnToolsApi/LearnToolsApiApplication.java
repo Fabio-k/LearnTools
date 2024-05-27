014b3649ccb9cd25b73385bbce7890b7b5c04aa2
fabio-k <fabio.guti@fatec.sp.gov.br>
2024-05-26 21:06:05 -0300

    feat: adiciona o backend da aplicação

---
+++ backend/src/main/java/com/LearnTools/LearnToolsApi/LearnToolsApiApplication.java
@@ -0,0 +1,13 @@
+package com.LearnTools.LearnToolsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToolsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnToolsApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/LearnTools/LearnToolsApi/LearnToolsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnTools.LearnToolsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnToolsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
