6b0f60bfbca024a9633a3db064a7e84b8f42fb0c
fabio-k <fabio.guti@fatec.sp.gov.br>
2024-08-11 16:27:23 -0300

    test: add tests to authentication, assistent and change the directory of flashcard service

--- backend/src/test/java/com/LearnTools/FlashcardServiceTest.java
+++ backend/src/test/java/com/LearnTools/FlashcardServiceTest.java
@@ -1,4 +1,4 @@
-package com.LearnTools.LearnToolsApi;
+package com.LearnTools;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
