e8289d434cf38527d48141c0a15fbaf7a9b32f52
fabio-k <fabio.guti@fatec.sp.gov.br>
2024-08-13 14:16:39 -0300

    refactor: atualiza o docker compose, atualiza o titulo da pagina e faz o vite rodar na porta 3000

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
   test: {
     globals: true,
     setupFiles: "src/setupTests.js",
